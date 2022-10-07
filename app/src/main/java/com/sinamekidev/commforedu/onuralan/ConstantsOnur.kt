e28cb792af9bc29033f5a2021fb7fc60fabe8ce0
OnurAlan20 <83537043+OnurAlan20@users.noreply.github.com>
2022-10-08 01:53:09 +0300

    Forum Design Bitti.

--- app/src/main/java/com/sinamekidev/commforedu/onuralan/ConstantsOnur.kt
+++ app/src/main/java/com/sinamekidev/commforedu/onuralan/ConstantsOnur.kt
@@ -4,3 +4,4 @@ import androidx.compose.ui.graphics.Color
 
 val blueLike = Color(0xFF4D5CA7)
 val topBar = Color(0xFF64254b)
+val frmTextColor = Color(0xFFBDBDBD)
--- app/src/main/java/com/sinamekidev/commforedu/onuralan/Forum.kt
+++ app/src/main/java/com/sinamekidev/commforedu/onuralan/Forum.kt
@@ -4,12 +4,18 @@ import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Absolute.SpaceAround
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -25,26 +31,60 @@ var text = ""
 
 @Composable
 fun ForumScreen() {
+
+    val txt1 = remember {
+        mutableStateOf("")
+    }
+    val txt2 = remember {
+        mutableStateOf("")
+    }
+    val txt3 = remember {
+        mutableStateOf("")
+    }
+    val txt4 = remember {
+        mutableStateOf("")
+    }
+    val txt5 = remember {
+        mutableStateOf("")
+    }
+    val txt6 = remember {
+        mutableStateOf("")
+    }
+    val txt7 = remember {
+        mutableStateOf("")
+    }
+    val txt8 = remember {
+        mutableStateOf("")
+    }
+    val txt9 = remember {
+        mutableStateOf("")
+    }
+    val txt10 = remember {
+        mutableStateOf("")
+    }
+
     Surface(modifier = Modifier.fillMaxSize(), color = Color(0XFF202124)) {
         Column(
             Modifier
                 .fillMaxWidth()
         ) {
-            Row(modifier = Modifier
+            Column(modifier = Modifier
                 .fillMaxWidth()
                 .height(60.dp)) {
                 ScafoldCompose()
             }
 
-            Row(modifier = Modifier
+            Column(modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 50.dp), horizontalArrangement = Arrangement.Center) {
+                .padding(top = 50.dp), horizontalAlignment = Alignment.CenterHorizontally) {
                 TxtField()
 
             }
-            Row(modifier = Modifier
+            Column(modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 30.dp, horizontal = 15.dp)) {
+                .padding(vertical = 30.dp, horizontal = 15.dp),
+                verticalArrangement = Arrangement.SpaceAround, horizontalAlignment = Alignment.Start) {
+                BestForumTopicsCard(txt1.value,txt2.value,txt3.value,txt4.value,txt5.value,txt6.value,txt7.value,txt8.value,txt9.value,txt10.value)
 
             }
 
@@ -60,11 +100,13 @@ fun ForumScreen() {
 fun ForumScreenTest(){
     ForumScreen()
 }
+
 @Composable
 fun ScafoldCompose(){
     Scaffold(
         topBar = { TopAppBarCompose()},
-        content = {ContentCompose()}
+        content = {ContentCompose()},
+
     )
 
 }
@@ -73,7 +115,8 @@ fun ScafoldCompose(){
 fun ContentCompose(){
     Box(modifier = Modifier
         .height(60.dp)
-        .fillMaxWidth())
+        .fillMaxWidth()
+        .padding(15.dp))
 
 }
 
@@ -123,4 +166,38 @@ fun TxtField(){
         },
 
         )
+}
+
+@Composable
+fun BestForumTopicsCard(txt1:String,
+                        txt2:String,
+                        txt3:String,
+                        txt4:String,
+                        txt5:String,
+                        txt6:String,
+                        txt7:String,
+                        txt8:String,
+                        txt9:String,
+                        txt10:String,
+                        ){
+    Card(modifier = Modifier.fillMaxSize(),
+        backgroundColor = Color.Transparent,
+        border = BorderStroke(width = 1.dp, color = Color.Green),
+        shape = RoundedCornerShape(corner = CornerSize(15.dp))
+        ) {
+        Column(modifier = Modifier.fillMaxSize().padding(15.dp), verticalArrangement = Arrangement.SpaceAround) {
+            Text(text = "1.$txt1", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "2.$txt2", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "3.$txt3", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "4.$txt4", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "5.$txt5", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "6.$txt6", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "7.$txt7", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "8.$txt8", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "9.$txt9", fontSize = 20.sp, color = Color.LightGray)
+            Text(text = "10.$txt10", fontSize = 20.sp, color = Color.LightGray)
+        }
+
+
+    }
 }
